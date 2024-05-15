234b033c35488e577550d5d935c0ab7fcea17a82
saadat-neosoft <saadat.badgujar@neosoftmail.com>
2024-05-15 23:24:36 +0530

    add home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,103 @@
 import Image from "next/image";
+import { MacbookScroll } from "./components/ui/Macbook-scroll";
+import { PlaceholdersAndVanishInput } from "./components/ui/PlaceholdersAndVanishInputDemo.jsx";
+import { HoverBorderGradient } from "./components/ui/HoverBorder";
+import { HeroHighlight, Highlight } from "./components/ui/HeroHighlight";
+import { InfiniteMovingCards } from "./components/ui/InfiniteMovingCards";
 
 export default function Home() {
+  const placeholders = [
+    "How much emergency fund should I have for rainy days?",
+    "What if I lose my job - do I have enough savings to sustain?",
+    "If interest rates rise, can I still afford my high EMIs?",
+    "How can I reduce taxable income from house property?",
+    "Should I invest in tax-free bonds or ELSS mutual funds?",
+    "Should I go for term or whole life insurance plan?",
+    "What should be my equity:debt asset allocation?",
+    "What tax saving options under 80C should I invest in this year?",
+    "Should I invest in PPF, NSC or other small savings schemes?",
+    "Do I need health insurance beyond what my employer provides?",
+    "How will I fund my retirement expenses after leaving my job?",
+  ];
+
+  const testimonials = [
+    {
+      quote: `Just took financial advice from 
+        @1FinanceHQ, a SEBI RIA
+        Really happy with the advice I got! Download the "1Finance" app and book a call with them
+        I understand that they're offering services for free right now
+        P.S. This is not a paid promo but a genuine shout out :)`,
+      name: "Kanan Bahl",
+      title: "@BahlKanan",
+    },
+    {
+      quote: `When I started earning money, one truth never stopped chasing me.
+      Unsolicited advice.
+      "Mutual funds, real estate, crypto & more”
+      All I needed at that point was a trusted financial strategy, no high commissions & no misled info.
+      Good thing that 
+      @1FinanceHQ
+      , doing exactly that.`,
+      name: "Ranveer Allahbadia",
+      title: "@BeerBicepsGuy",
+    },
+    {
+      quote:
+        "Be it Finance or Life, there are very few people who are looking out for your interests. In my entire life, I have fallen for numerous ill advised financial schemes. But glad now there are platforms like 1Finance, great initiative 👍",
+      name: "Boman Irani",
+      title: "@bomanirani",
+    },
+    {
+      quote: `Mis-selling of financial products is an unfortunate reality. Great to see a brand raising awareness about such an issue which is widespread but rarely spoken about …
+        @1FinanceHQ`,
+      name: "Neha Nagar",
+      title: "@nehanagarr",
+    },
+    {
+      quote:
+        "Having worked in a bank for 11 years, I can relate to this feeling - that we are selling what the bank wants us to sell. It should be about the customer, not the seller. Great video. Funny too. #1finance",
+      name: "Chetan Bhagat",
+      title: "@chetan_bhagat",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex flex-col justify-center items-center h-screen">
+      <HeroHighlight>
+        <div className="flex flex-col justify-center items-center h-svh w-full gap-3">
+          {/* <MacbookScroll /> */}
+          <div className="font-sans">All-in-one financial advisory app</div>
+          <div className="font-serif text-5xl text-center leading-tight">
+            Get
+            <br />
+            <Highlight>Tax</Highlight>
+            <br />
+            Planning
+          </div>
+          <div className="flex justify-center items-center w-full">
+            <PlaceholdersAndVanishInput placeholders={placeholders} />
+          </div>
+          <div className="flex justify-center text-center cursor-pointer">
+            <HoverBorderGradient
+              containerClassName="rounded-full"
+              as="button"
+              className="bg-black text-white flex items-center space-x-2 cursor-pointer"
+            >
+              <span className="font-sans">Download the app</span>
+            </HoverBorderGradient>
+          </div>
+          <div className="w-screen overflow-x-hidden">
+            <InfiniteMovingCards
+              items={testimonials}
+              direction="right"
+              speed="slow"
             />
-          </a>
+          </div>
         </div>
-      </div>
+        {/* <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden"> */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {/* </div> */}
+      </HeroHighlight>
+    </div>
   );
 }
